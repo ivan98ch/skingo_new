01f1903b15879e453074a84b750b59da896fd7da
Oriol Gavalda <oriolgavalda3@gmail.com>
2020-05-11 19:13:16 +0200

    TEST
    Oga

--- src/app/guards/home.guard.ts
+++ src/app/guards/home.guard.ts
@@ -1,26 +1,30 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { StorageService } from '../services/storage.service';
-import { Observable } from 'rxjs';
-import { AuthService } from '../services/auth.service';
-import { tap, map, take } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { map } from 'rxjs/operators';
+import { isNullOrUndefined } from 'util';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeGuard implements CanActivate {
-  constructor(public auth: AuthService, public router: Router) {}
+  constructor(private afAuth: AngularFireAuth, public router: Router) {}
 
-  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return this.auth.user$.pipe(
-         take(1),
-         map(user => !!user),
-         tap(loggedIn => {
-           if (!loggedIn) {
-             console.log('access denied');
-             this.router.navigate(['/login']);
-           }
-       })
-    );
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+
+    return  this.afAuth.authState.pipe(map( auth => {
+
+      if (isNullOrUndefined(auth)) {
+        this.router.navigate(['/login']);
+        return false;
+      } else {
+        return true;
+      }
+
+    } ));
+
   }
+
 }
+
+// this.router.navigate(['/login']);
